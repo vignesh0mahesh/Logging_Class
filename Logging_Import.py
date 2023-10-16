55aa584beaf0f3d23376c2a1b4de896ce0ec9191
vignesh0mahesh <106109521+vignesh0mahesh@users.noreply.github.com>
2023-10-15 20:38:05 -0700

    Add files via upload

--- Logging_Import.py
+++ Logging_Import.py
@@ -10,6 +10,12 @@ from colors import *
 
 class LogUpload:
     def __init__(self, log_name, log_level='Print'):
+        """This is the initialization function for the Log Upload Class
+
+        Args:
+            log_name (str): Enter the name you would like the log file to have.
+            log_level (str, optional): Enter the type of logging you would like to see. Options Are: 'Silent' , 'Print' or 'Debug'. Defaults to 'Print'.
+        """
         # log_level = 'Silent' , 'Print' or 'Debug'
         
         log_file = datetime.now().strftime("%d_%m_%Y-%H-%M-%S") + f"_{log_name}_{log_level}_Log.log"
@@ -44,8 +50,13 @@ class LogUpload:
         
         print(color("Logging Class Sucessfully Imported", fg='green'))
         
-            
     def initialize_loki(self, url, system_name):
+        """This function is used to initialize a connection to your loki logging server. 
+
+        Args:
+            url (str): Enter the url to your loki logging server instance, should be in the following format : "https://my-loki-instance/loki/api/v1/push"
+            system_name (str): Enter a name that will be used as a tag for your system, so that you can filter for this specific system in Grafana Loki. 
+        """
         try:
             
             handler = logging_loki.LokiHandler(url=url, version="1",)
@@ -55,35 +66,60 @@ class LogUpload:
             self.user_tag = os.environ['USERNAME']
             self.default_tag = {"SYSTEM": "", "USER": "", "MODULE": "", "CLASS": "", "FUNCTION": ""}
             self.tags = self.default_tag
-            self.append_tags({"SYSTEM": self.system_tag, "USER": self.user_tag})
+            self._append_tags({"SYSTEM": self.system_tag, "USER": self.user_tag})
             self.log_upload = True
             print(color('Loki Logging Initialized', fg='green'))
 
         except:
-            print(color('Log Function Import Unsucessfull', fg='red', style='bold'))
+            print(color('Log Function Import Unsuccessful', fg='red', style='bold'))
 
-    def append_tags(self, tags):
+    def _append_tags(self, tags):
+        """This is an internal function that appends tags from tag functions below. If you want to add more custom tags, invoke this function.
+
+        Args:
+            tags (dict): Dictionary with {tag name: tag value format}
+        """
         for key in tags:
             self.tags[key] = tags[key]
 
-    def module_tag(self, module_name):
-        self.append_tags({"MODULE": module_name})
+    def _module_tag(self, module_name):
+        """Appends the module (if exists) from which this log function is called from in your code to the tag. Automatically generated, not user defined. 
 
-    def class_tag(self, class_name):
-        self.append_tags({"CLASS": class_name})
+        Args:
+            module_name (str): Name of the module the log function is being called from
+        """
+        self._append_tags({"MODULE": module_name})
 
-    def function_tag(self, function_name):
-        self.append_tags({"FUNCTION": function_name})
+    def _class_tag(self, class_name):
+        """Appends the class (if exists) from which this log function is called from in your code to the tag. Automatically generated, not user defined.
+
+        Args:
+            class_name (str): Name of the class the log function is being called from
+        """
+        self._append_tags({"CLASS": class_name})
+
+    def _function_tag(self, function_name):
+        """Appends the function (if exists) from which this log function is called from in your code to the tag. Automatically generated, not user defined.
+
+        Args:
+            function_name (str): Name of the function the log function is being called from
+        """
+        self._append_tags({"FUNCTION": function_name})
 
     def debug(self, debug_string: str):
+        """To log a debug level message
+
+        Args:
+            debug_string (str): Message to log
+        """
 
         caller_frame = inspect.stack()[1]
 
-        self.module_tag(inspect.getmodule(caller_frame[0]).__name__)
+        self._module_tag(inspect.getmodule(caller_frame[0]).__name__)
 
-        self.class_tag(caller_frame[0].f_locals.get('self', None).__class__.__name__)
+        self._class_tag(caller_frame[0].f_locals.get('self', None).__class__.__name__)
 
-        self.function_tag(caller_frame[0].f_code.co_name)
+        self._function_tag(caller_frame[0].f_code.co_name)
         
         if self.log_upload:
             self.loki_logger.debug(debug_string, extra={"tags": self.tags})
@@ -95,17 +131,21 @@ class LogUpload:
             
         else:
             self.logger.debug(debug_string)
-        
 
     def info(self, info_string: str):
+        """To log a info level message
+
+        Args:
+            info_string (str): Message to log
+        """
 
         caller_frame = inspect.stack()[1]
 
-        self.module_tag(inspect.getmodule(caller_frame[0]).__name__)
+        self._module_tag(inspect.getmodule(caller_frame[0]).__name__)
 
-        self.class_tag(caller_frame[0].f_locals.get('self', None).__class__.__name__)
+        self._class_tag(caller_frame[0].f_locals.get('self', None).__class__.__name__)
 
-        self.function_tag(caller_frame[0].f_code.co_name)
+        self._function_tag(caller_frame[0].f_code.co_name)
         
         if self.log_upload:
             self.loki_logger.info(info_string, extra={"tags": self.tags})
@@ -120,17 +160,21 @@ class LogUpload:
             
         else:
             self.logger.info(info_string)
-            
 
-    def warning(self, warn_string):
+    def warning(self, warn_string: str):
+        """To log a warning level message
+
+        Args:
+            warn_string (str): Message to log
+        """
 
         caller_frame = inspect.stack()[1]
 
-        self.module_tag(inspect.getmodule(caller_frame[0]).__name__)
+        self._module_tag(inspect.getmodule(caller_frame[0]).__name__)
 
-        self.class_tag(caller_frame[0].f_locals.get('self', None).__class__.__name__)
+        self._class_tag(caller_frame[0].f_locals.get('self', None).__class__.__name__)
 
-        self.function_tag(caller_frame[0].f_code.co_name)
+        self._function_tag(caller_frame[0].f_code.co_name)
 
         if self.log_upload:
             self.loki_logger.warning(warn_string, extra={"tags": self.tags})
@@ -146,15 +190,20 @@ class LogUpload:
         else:
             self.logger.warning(warn_string)
 
-    def error(self, err_string):
+    def error(self, err_string: str):
+        """To log a error level message
+
+        Args:
+            err_string (str): Message to log
+        """
         
         caller_frame = inspect.stack()[1]
 
-        self.module_tag(inspect.getmodule(caller_frame[0]).__name__)
+        self._module_tag(inspect.getmodule(caller_frame[0]).__name__)
 
-        self.class_tag(caller_frame[0].f_locals.get('self', None).__class__.__name__)
+        self._class_tag(caller_frame[0].f_locals.get('self', None).__class__.__name__)
 
-        self.function_tag(caller_frame[0].f_code.co_name)
+        self._function_tag(caller_frame[0].f_code.co_name)
 
         if self.log_upload:
             self.loki_logger.error(err_string, extra={"tags": self.tags})
@@ -169,17 +218,21 @@ class LogUpload:
             
         else:
             self.logger.error(err_string)
-            
-    
-    def critical(self, crit_string):
+
+    def critical(self, crit_string: str):
+        """To log a critical level message
+
+        Args:
+            crit_string (str): Message to log
+        """
 
         caller_frame = inspect.stack()[1]
 
-        self.module_tag(inspect.getmodule(caller_frame[0]).__name__)
+        self._module_tag(inspect.getmodule(caller_frame[0]).__name__)
 
-        self.class_tag(caller_frame[0].f_locals.get('self', None).__class__.__name__)
+        self._class_tag(caller_frame[0].f_locals.get('self', None).__class__.__name__)
 
-        self.function_tag(caller_frame[0].f_code.co_name)
+        self._function_tag(caller_frame[0].f_code.co_name)
 
         if self.log_upload:
             self.loki_logger.critical(crit_string, extra={"tags": self.tags})
